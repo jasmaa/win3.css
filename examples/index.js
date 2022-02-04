95fbed0d1b05576d7d3189550ca847cb8b19a87c
jasmaa <jasonmaa3955@gmail.com>
2022-02-03 23:48:15 -0500

    Make demo window draggable

---
+++ examples/index.js
@@ -0,0 +1,40 @@
+// Adapted from: https://www.w3schools.com/howto/howto_js_draggable.asp
+const windowFrame = document.getElementById('window-frame')
+const windowHeader = document.getElementById('window-header');
+
+document.body.style.overflow = 'hidden';
+initializeDraggableWindow(windowFrame, windowHeader);
+
+function initializeDraggableWindow(windowFrame, windowHeader) {
+  windowFrame.style.position = 'absolute';
+  windowFrame.style.top = '8em';
+  windowHeader.style.cursor = 'move';
+
+  let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+  windowHeader.onmousedown = e => {
+    if (e.target === windowHeader) {
+      e.preventDefault();
+
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+
+      document.onmousemove = e => {
+        e.preventDefault();
+        // get mouse position
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        windowFrame.style.top = (windowFrame.offsetTop - pos2) + "px";
+        windowFrame.style.left = (windowFrame.offsetLeft - pos1) + "px";
+      }
+
+      document.onmouseup = () => {
+        document.onmouseup = null;
+        document.onmousemove = null;
+      }
+    }
+  }
+}
